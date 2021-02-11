6c05d78dc918aa55fcbc560e1c30acd58b4f7ddb
Camilo Castro <kamilo_eternal@hotmail.com>
2021-02-11 17:41:42 -0500

    jwt roles

--- FilesApi.Utilities/Response/UserResponse.cs
+++ FilesApi.Utilities/Response/UserResponse.cs
@@ -2,6 +2,7 @@
 {
     public class UserResponse
     {
+        public string message { get; set; }
         public string token { get; set; }
         public string userId { get; set; }
         public string role { get; set; }
